66050b8b9aad316794954b1531e8ec2cd5d24b9d
Nicolai Hygino <nicolaihygino2000@gmail.com>
2025-02-14 07:53:23 -0300

    feat: creates configuration package and configure CORS

--- src/main/java/com/example/multiplication/challenge/ChallengeServiceImpl.java
+++ src/main/java/com/example/multiplication/challenge/ChallengeServiceImpl.java
@@ -1,21 +1,23 @@
 package com.example.multiplication.challenge;
 
 import com.example.multiplication.user.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChallengeServiceImpl implements ChallengeService {
-    @Override
-    public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
-        // Check if the attempt is correct
-        boolean isCorrect = attemptDTO.getGuess() == attemptDTO.getFactorA() * attemptDTO.getFactorB();
-        // We don't use identifiers for now
-        User user = new User(null, attemptDTO.getUserAlias());
-        // Builds the domain object. Null if for now.
-        return new ChallengeAttempt(null,
-                user.getId(),
-                attemptDTO.getFactorA(),
-                attemptDTO.getFactorB(),
-                attemptDTO.getGuess(),
-                isCorrect
-        );
-    }
+  @Override
+  public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
+    // Check if the attempt is correct
+    boolean isCorrect = attemptDTO.getGuess() == attemptDTO.getFactorA() * attemptDTO.getFactorB();
+    // We don't use identifiers for now
+    User user = new User(null, attemptDTO.getUserAlias());
+    // Builds the domain object. Null if for now.
+    return new ChallengeAttempt(
+        null,
+        user.getId(),
+        attemptDTO.getFactorA(),
+        attemptDTO.getFactorB(),
+        attemptDTO.getGuess(),
+        isCorrect);
+  }
 }
